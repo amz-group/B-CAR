b1d033d68bd6d9907ee4d18a64587ec544076c1d
KRD GROUP For Developer <118355701+amz-group@users.noreply.github.com>
2026-09-03 00:46:08 +0300

    Format vite.config.js for better readability

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-export default defineConfig({ plugins: [react()], resolve: { alias: { '@': new URL('./src', import.meta.url).pathname } } });
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
+});
